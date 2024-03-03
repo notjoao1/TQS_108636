4c5b37dd116164c11c10387ca13c848cd6ac6d96
notjoao1 <joao.dourado1@ua.pt>
2024-03-03 17:49:02 +0000

    lab3: ex3 done

---
+++ lab3/lab3_3cars_realDB/src/main/java/com/tqs108636/lab3_2cars/Lab32carsApplication.java
@@ -0,0 +1,13 @@
+package com.tqs108636.lab3_2cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab32carsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab32carsApplication.class, args);
+	}
+
+}
