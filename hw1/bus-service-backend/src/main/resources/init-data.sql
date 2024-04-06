928b7ae519e09b3dfd3f3dba74e6ea774256b52a
notjoao1 <joao.dourado1@ua.pt>
2024-04-06 13:07:29 +0100

    hw1-backend: fixed initialization data timestamps + added reservation

--- hw1/bus-service-backend/src/main/resources/init-data.sql
+++ hw1/bus-service-backend/src/main/resources/init-data.sql
@@ -60,29 +60,34 @@ INSERT INTO route_stop (location_id, route_id, stop_number, distance_km_from_las
 
 
 INSERT INTO trip (route_id, departure_time, price_euro, number_of_seats)
-    VALUES (1, FROM_UNIXTIME(1711316223), 15, 20);
+    VALUES (1, FROM_UNIXTIME(1713916223), 15, 20);
 INSERT INTO trip (route_id, departure_time, price_euro, number_of_seats)
     VALUES (1, FROM_UNIXTIME(1711057023), 10, 20);
 INSERT INTO trip (route_id, departure_time, price_euro, number_of_seats)
     VALUES (2, FROM_UNIXTIME(1710279423), 8, 15);
 INSERT INTO trip (route_id, departure_time, price_euro, number_of_seats)
-    VALUES (2, FROM_UNIXTIME(1712093823), 6, 10);
+    VALUES (2, FROM_UNIXTIME(1712993823), 6, 10);
 INSERT INTO trip (route_id, departure_time, price_euro, number_of_seats)
-    VALUES (2, FROM_UNIXTIME(1711967823), 10, 20);
+    VALUES (2, FROM_UNIXTIME(1714967823), 10, 20);
 INSERT INTO trip (route_id, departure_time, price_euro, number_of_seats)
-    VALUES (3, FROM_UNIXTIME(1711535823), 15, 15);
+    VALUES (3, FROM_UNIXTIME(1713535823), 15, 15);
 INSERT INTO trip (route_id, departure_time, price_euro, number_of_seats)
-    VALUES (3, FROM_UNIXTIME(1712093823), 20, 30);
+    VALUES (3, FROM_UNIXTIME(1715093823), 20, 30);
 INSERT INTO trip (route_id, departure_time, price_euro, number_of_seats)
-    VALUES (3, FROM_UNIXTIME(1711967823), 10, 20);
+    VALUES (3, FROM_UNIXTIME(1719967823), 10, 20);
 INSERT INTO trip (route_id, departure_time, price_euro, number_of_seats)
-    VALUES (4, FROM_UNIXTIME(1711967823), 10, 20);
+    VALUES (4, FROM_UNIXTIME(1719967823), 10, 20);
 INSERT INTO trip (route_id, departure_time, price_euro, number_of_seats)
-    VALUES (5, FROM_UNIXTIME(1711967823), 5, 20);
+    VALUES (5, FROM_UNIXTIME(1719967823), 5, 20);
 INSERT INTO trip (route_id, departure_time, price_euro, number_of_seats)
     VALUES (5, FROM_UNIXTIME(1709289423), 10, 20);
 INSERT INTO trip (route_id, departure_time, price_euro, number_of_seats)
-    VALUES (5, FROM_UNIXTIME(1711967823), 10, 20);
+    VALUES (5, FROM_UNIXTIME(1719967823), 10, 20);
 INSERT INTO trip (route_id, departure_time, price_euro, number_of_seats)
     VALUES (6, FROM_UNIXTIME(1714559823), 20, 40);
+
+-- Trip 1 - seat number 7 taken
+INSERT INTO reservation (id, trip_id, seat_number, client_name)
+    VALUES (UNHEX('3ba26311323d4d2db12dd77dde16ca17'), 1, 7, 'Cliente Estatico Fixe'); 
+
 
